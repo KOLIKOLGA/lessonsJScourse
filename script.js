d44b00b96f15dd6f374d9fa6613999657ba9a3f2
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2023-12-20 19:32:40 +0300

    add values ​​to variables

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Калькулятор верстки";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1000;
+let rollback = 25;
+let fullPrice = 10000;
+let adaptive = true;
 
 alert("Hello friend! How are you?");
 console.log("Hello world!!!");
