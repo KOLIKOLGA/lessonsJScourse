bb5808c8c5bbd593ec98057d3d0fb379e4136cde
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2023-12-20 20:23:57 +0300

    using methods and properties output to the console.log()

--- script.js
+++ script.js
@@ -5,5 +5,18 @@ let rollback = 25;
 let fullPrice = 10000;
 let adaptive = true;
 
-alert("Hello friend! How are you?");
+//alert("Hello friend! How are you?");
 console.log("Hello world!!!");
+//1)
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+//2)
+console.log(screens.length);
+let concot = "Стоимость верстки экранов " + screenPrice + " $";
+console.log(concot);
+concot = "Стоимость разработки сайта " + fullPrice + " $";
+console.log(concot);
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+console.log(fullPrice * (rollback / 100));
