b84d86dfb6f36afdba77e28618881e62eaeeb8c0
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2023-12-22 05:41:22 +0300

    delete coments

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ let rollback = 25;
 let fullPrice = 10000;
 let adaptive = true;
 
-//alert("Hello friend! How are you?");
 console.log("Hello world!!!");
-//1)
+
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
-//2)
+
 console.log(screens.length);
-let concot = "Стоимость верстки экранов " + screenPrice + " $";
-console.log(concot);
-concot = "Стоимость разработки сайта " + fullPrice + " $";
-console.log(concot);
+
+console.log("Стоимость верстки экранов " + screenPrice + " $");
+
+console.log("Стоимость разработки сайта " + fullPrice + " $");
 console.log(screens.toLowerCase());
 console.log(screens.split(", "));
 console.log(fullPrice * (rollback / 100));
