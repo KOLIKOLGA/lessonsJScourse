63705bde1656ad66a1b9c40cab1b08888f2c3b3a
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2023-12-29 20:46:40 +0300

    servicePrecentPrice=Math.ceil(fullPrice - fullPrice*(rollBack/100))

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let screens = prompt(
   "Простые, Сложные, Интерактивные"
 );
 let screenPrice = +prompt("Сколько будет стоить данная работа", "12000");
-let rollback = 25;
+let rollback = 10;
 let adaptive = confirm("Нужен ли адаптив на сайте");
 let service1 = prompt("Какой дополнительный тип услуги нужен?", "Каталог");
 let servicePrice1 = +prompt("Сколько это будет стоить?", "1000");
 let service2 = prompt("Какой дополнительный тип услуги нужен?", "Каталог");
 let servicePrice2 = +prompt("Сколько это будет стоить?", "1000");
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = Math.ceil(fullPrice * (rollback / 100));
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
 if (fullPrice >= 30000) {
   console.log("Даем скидку в 10%");
