627da6a65653ed0cedeb952cf9cc7800a0a70767
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2023-12-30 19:43:41 +0300

    start branch lesson04

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ let screens = prompt(
   "Какие типы экранов нужно разработать?",
   "Простые, Сложные, Интерактивные"
 );
-let screenPrice = +prompt("Сколько будет стоить данная работа", "12000");
+let screenPrice = +prompt("Сколько будет стоить данная работа", "20000");
 let rollback = 10;
 let adaptive = confirm("Нужен ли адаптив на сайте");
 let service1 = prompt("Какой дополнительный тип услуги нужен?", "Каталог");
-let servicePrice1 = +prompt("Сколько это будет стоить?", "1000");
+let servicePrice1 = +prompt("Сколько это будет стоить?", "11000");
 let service2 = prompt("Какой дополнительный тип услуги нужен?", "Каталог");
-let servicePrice2 = +prompt("Сколько это будет стоить?", "1000");
+let servicePrice2 = +prompt("Сколько это будет стоить?", "11000");
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
 if (fullPrice >= 30000) {
   console.log("Даем скидку в 10%");
-} else if (fullPrice >= 15000) {
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
   console.log("Даем скидку в 5%");
-} else if (fullPrice < 15000) {
+} else if (fullPrice > 0 && fullPrice < 15000) {
   console.log("Скидка не предусмотрена");
 } else if (fullPrice < 0) {
   console.log("Что то пошло не так");
